8fd9ff83c69f00dc2567baf65ba5c068d5cba415
justaguy12345 <ryanberk135@gmail.com>
2023-08-01 22:11:37 -0500

    error testing response keys

--- initiatorInterface.py
+++ initiatorInterface.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+from file_tools import *
 py = "python3 -u "
 AtomicSwapECCPath = "Ergo/SigmaParticle/AtomicMultiSigECC/py/deploy.py " #TODO Ergo Specific
 s_ = " "
@@ -8,7 +8,7 @@ s_ = " "
 def initiation(crossChainPubKey, initiatorChain): #returns an initiation JSON object #TODO link between selected chain and atomic ECC
     return os.popen(py + AtomicSwapECCPath + "p1Initiate " + crossChainPubKey + s_ + initiatorChain).read()
 
-def sanitizeInitiation(initiationJSON):
+def sanitizeInitiation(initiationJSON): #this can be done at the SigmaParticle framework level easily instead of here
     j = json.loads(initiationJSON)
     ksG = j["ksG"]
     ks = j["ks"]
@@ -25,5 +25,17 @@ def sanitizeInitiation(initiationJSON):
             .rstrip()
     return strip1
 
+def inspectResponse(DEC_response_filepath, responderinfo_filepath):
+    j_response = json.loads(clean_file_open(DEC_response_filepath, "r"))
+    if "chain" not in j_response or "contractAddr" not in j_response:
+        return print("Error: response does not have expected keys")
+    else:
+        fundedAmount = Atomicity_CheckContractFunds(j_response)
 
 
+
+
+def Atomicity_CheckContractFunds(j_response):
+    chain = j_response["chain"]
+    contractAddr = j_response["contractAddr"]
+    
--- main.py
+++ main.py
@@ -7,16 +7,22 @@ args = sys.argv
 
 
 def test():
+    testkey = "688344026718772736449750175203366052782498205293898002465375827258042277361951460658218874759221293994168145022574766874751338527256700500579101512082414055194093613376114567923022297129476978722630282962906957224675125386874494158492157124310481876254258350563100432848938338097941551681473725391869419801716664372453775554757712481751968704577158437846771260413284009770218290762832891954510055886590737"
+    testkeypath = "Key0.ElGamalKey"
+
     privateInit = initiation("0xFe4cc19ea6472582028641B2633d3adBB7685C69", "Ergo")
     print(privateInit)
     publicInit = sanitizeInitiation(privateInit)
     print(publicInit)
-    testkey = "688344026718772736449750175203366052782498205293898002465375827258042277361951460658218874759221293994168145022574766874751338527256700500579101512082414055194093613376114567923022297129476978722630282962906957224675125386874494158492157124310481876254258350563100432848938338097941551681473725391869419801716664372453775554757712481751968704577158437846771260413284009770218290762832891954510055886590737"
-    encrypt = ElGamal_Encrypt(testkey, "Key0.ElGamalKey", publicInit, "Test_Encryption.bin")
+    encrypt = ElGamal_Encrypt(testkey, testkeypath, publicInit, "Test_Encryption.bin")
     ENC_init = clean_file_open("Test_Encryption.bin", "r")
-    process_initiation(ENC_init, "TestResponseEncryptedPath.bin", "TestResponseDecryptedPath.bin", testkey, "Key0.ElGamalKey")
+    process_initiation(ENC_init, "TestResponseEncryptedPath.bin", "TestResponseDecryptedPath.bin", testkey, testkeypath)
     response("TestResponseDecryptedPath.bin", "sr_path_test.bin", "x_path_test.bin", \
-            "response_path_test.bin",  "ENC_response_path_test.bin", testkey, "Key0.ElGamalKey")
+            "response_path_test.bin",  "ENC_response_path_test.bin", testkey, testkeypath)
+    decrypted_response = ElGamal_Decrypt("ENC_response_path_test.bin", testkey, testkeypath)
+    print(decrypted_response)
+    clean_file_open("DEC_response_path_test.bin", "w", decrypted_response)
+    inspectResponse("DEC_response_path_test.bin", "responderinfo_test.json")
     
 
 if len(args) >= 1:
